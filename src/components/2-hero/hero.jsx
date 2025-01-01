9bcf101434c0b847ca1f6ced0c64d5092c58c8ed
Said Azmour <azmour2016maroc@gmail.com>
2025-01-01 20:24:55 +0100

    add hero section

--- src/components/2-hero/hero.jsx
+++ src/components/2-hero/hero.jsx
@@ -2,9 +2,25 @@ import './hero.css';
 
 function Hero() {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='Hero'>
+      <div className='Hero__container'>
+        <img src="/80750.png" alt="" />
+        <div className='herocontent'>
+          <span className=''>Hi, im Said &#128075;</span>
+          <h1 className=''>Full stack web developer</h1>
+          <p>I code beautifully simple things, and I love what I do.</p>
+        </div> 
+        <ul className='flex links'>
+          <li><a href='#' className='icon-twitter'></a></li>
+          <li><a href='#' className='icon-instagram'></a></li>
+          <li><a href='#' className='icon-github'></a></li>
+          <li><a href='#' className='icon-linkedin'></a></li>
+        </ul>
+        <button className='icon-keyboard_arrow_up top-animation'></button>
+      </div> 
+
     </div>
+    
   )
 }
 
--- src/components/3-main/main.jsx
+++ src/components/3-main/main.jsx
@@ -2,9 +2,13 @@ import './main.css';
 
 function Main() {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+      <div className='about'>
+        <img src="/public/about.png" alt="about"  className='border'/>
+        <div className='about-content'>
+          <h2> About Me </h2>
+          <p> We are a team of passionate individuals who are dedicated to creating innovative solutions for our </p>
+        </div>
+      </div>
   )
 }
 
