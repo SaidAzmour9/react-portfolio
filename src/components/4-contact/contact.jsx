5a4ff016b7e19de6a4fad518553a0285d4ebc3fa
Said Azmour <azmour2016maroc@gmail.com>
2025-01-02 03:01:12 +0100

    add main section

--- src/components/4-contact/contact.jsx
+++ src/components/4-contact/contact.jsx
@@ -3,8 +3,14 @@ import './contact.css';
 function Contact() {
   return (
     <div>
-      
-    </div>
+      <h3>Professional Skillset</h3>
+        <div>
+            <div className="skillset-item">HTML/CSS</div>
+            <div className="skillset-item">JavaScript</div>
+            <div className="skillset-item">React</div>
+            <div className="skillset-item">Node.js</div>
+          </div>
+        </div>
   )
 }
 
