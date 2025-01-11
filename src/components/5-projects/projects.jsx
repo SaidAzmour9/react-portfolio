fa9d3eea614af3a8dfee22ce62d1010f0d55433c
Said Azmour <azmour2016maroc@gmail.com>
2025-01-11 03:31:53 +0100

    add projects part1

--- src/components/5-projects/projects.jsx
+++ src/components/5-projects/projects.jsx
@@ -4,29 +4,42 @@ import { useState } from 'react';
 
 const myProjects = [
   {
-    name: 'Project 1',
-    description: 'This is a description of project 1.',
+    name: 'Rezilla landing page',
+    description: 'A landing page for a fictional company Assisting individuals in locating the appropriate real estate.',
     category: 'front',
-    image: 'https://picsum.photos/200/300',
-    },
-    {
-      name: 'Project 2',
-      description: 'This is a description of project 2.',
-      category: 'back',
-      image: 'https://picsum.photos/200/301',
-      },
-      {
-        name: 'Project 3',
-        description: 'This is a description of project 3.',
-        category: 'full',
-        image: 'https://picsum.photos/200/302',
-        },
-        {
-          name: 'Project 4',
-          description: 'This is a description of project 3.',
-          category: 'full',
-          image: 'https://picsum.photos/200/302',
-          },
+    image: '/rezilla.png',
+    link: 'https://rezilla-chi.vercel.app/',
+    github: 'https://github.com/SaidAzmour9/rezilla',
+    tags : ['html','css', 'javascript']
+  },
+  {
+    name: 'Todo list',
+    description: 'A simple todo list application that allows users to add, remove, and mark tasks completed',
+    category: 'front',
+    image: '/todo.png',
+    link: 'https://maradskrgh.netlify.app/',
+    github: 'https://github.com/SaidAzmour9/todo-list-app',
+    tags : ['html','css', 'javascript']
+  },
+  {
+    name: 'Password generator',
+    description: 'A password generator that generates a random password based on user choice.',
+    category: 'front',
+    image: '/pass.png',
+    link: 'https://legendary-torrone-1b088a.netlify.app/',
+    github: 'https://github.com/SaidAzmour9/passgen',
+    tags : ['html','tailwindcss', 'jquery']
+  },
+  {
+    name: 'Team landing page',
+    description: 'A landing page for a fictional company that provides team collaboration tools.',
+    category: 'front',
+    image: '/team.png',
+    link: 'https://teampage-tailwind.netlify.app/',
+    github: 'https://github.com/SaidAzmour9/team-page',
+    tags : ['html','tailwindcss']
+  },
+   
 ]
 
 
@@ -72,19 +85,19 @@ function Projects() {
           return (
             <div className="card" key={item.image}>
             <div className="card-img">
-              <img src="https://picsum.photos/300/200" alt="Card image cap"/>
+              <img src={item.image} alt="Card image cap"/>
               <div className='tags'>
-                <span>Html</span>
-                <span>css</span>
-                <span>javascript</span>
+              {item.tags && item.tags.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
               </div>
             </div>
             <div className="card-body">
               <h5 className="card-title">{item.name}</h5>
               <p className="card-text">{item.description}</p>
               <div className="alinks">
-                <a href="#" className='icon-github'></a>
-                <a href="#" className='icon-github'></a>
+              <a href={item.github} className='icon-github' target="_blank" rel="noopener noreferrer"></a>
+                <a href={item.link} className='icon-chain' target="_blank" rel="noopener noreferrer"></a>
               </div>
             </div>
             </div>
